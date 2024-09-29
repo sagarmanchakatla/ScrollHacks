8cda3eccda29c23a416c23926efbe9fbcc05218d
Sagar Manchakatla <sagarmanchakatla01@gmail.com>
2024-09-29 17:59:11 +0530

    Name change

--- financial_help/client/vite.config.js
+++ financial_help/client/vite.config.js
@@ -1,22 +1,46 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://scrollhacks-fkqq.onrender.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate", // Automatically updates the service worker
+      includeAssets: [
+        "favicon.svg",
+        "favicon.ico",
+        "robots.txt",
+        "apple-touch-icon.png",
+      ], // Icons and assets
+      manifest: {
+        name: "FinPlanPro", // Name of the app
+        short_name: "Learn about finance with AI", // Short name for the app
+        description: "Help with financial planning",
+        theme_color: "#ffffff", // Theme color
+        background_color: "#ffffff", // Background color
+        display: "standalone", // App will display standalone like a native app
+        scope: "/",
+        start_url: "/",
+        icons: [
+          {
+            src: "icon-192x192.png", // Path to your icons
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-512x512.png", // Path to your icons
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
       },
-    },
-  },
+    }),
+  ],
 });
